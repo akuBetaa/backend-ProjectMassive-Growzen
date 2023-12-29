d4039aa31edfa20b26058051ba83af46a98d0b2d
Beta Nurul Awwalin <beta.n.awwalin@gmail.com>
2023-12-29 14:34:47 +0700

    try again and again

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
 import mysql from "mysql2";
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    database: process.env.DB_DBNAME,
-    password: process.env.DB_PASSWORD
-});
-
 // const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     database: 'webtbc_db',
-//     password: '1234'
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     database: process.env.DB_DBNAME,
+//     password: process.env.DB_PASSWORD
 // });
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'webtbc_db',
+    password: '1234'
+});
 
 db.connect(function (err) {
     if (err) {
